d607bb250ce53affd22798519845e813b4fb62e4
Vince <vinceawisan59@gmail.com>
2022-09-22 04:53:53 +0800

    styled

--- index.js
+++ index.js
@@ -5,29 +5,60 @@
   let eraser = document.querySelector(".eraser")
   const clr = document.querySelector(".clear-btn")
   let gridCont = document.querySelector(".pad")
- 
-  html.addEventListener('dragstart', (e) => {
-    e.preventDefault()
-  })
+  let sc = document.querySelector(".s-container")
+  let sl = document.querySelector(".slider")
+  let colorr = document.querySelector("#color");
+
   
-  html.addEventListener('drop', (e) => {
-    e.preventDefault()
-  })
- 
 function x(defSize){
 
-    let rand = false;
-    let k = false
-    let erase = false; 
+    newBtn.addEventListener("click",()=>{
+        grids.forEach(grid =>{
+           grid.remove();
+        })
+       })
    
-  
+  let k = false
+  let erase = false; 
+  let neon = true;
+
+  gridCont.addEventListener("mousedown", ()=>{
+      k = true;
+      
+     
+  }) 
+
+  gridCont.addEventListener("mouseup", ()=>{
+      k = false;
+      
+  }) 
+  gridCont.addEventListener("mouseleave", ()=>{
+      k = false;
+      
+  }) 
+  sc.addEventListener("click",()=>{
+      neon = !neon;
+  })
+ 
+eraser.addEventListener("click",()=>{
+ erase = !erase
+ 
+})
+html.addEventListener('dragstart', (e) => {
+  e.preventDefault()
+})
+
+html.addEventListener('drop', (e) => {
+  e.preventDefault()
+})
+
 
        for(i=0;i<`${(defSize)}` ** 2;i++){
             let p = document.createElement("div"); 
             p.classList.add("grid");           
             gridCont.appendChild(p)
         }
-
+       
         
         gridCont.style.gridTemplateColumns = `repeat(${defSize},1fr)`;
         gridCont.style.gridTemplateRows = `repeat(${defSize},1fr)`;
@@ -38,69 +69,50 @@ function x(defSize){
            
         
 
-            newBtn.addEventListener("click",()=>{
-                 grids.forEach(grid =>{
-                    grid.remove();
-                 })
-                })
+            
    
-    
-
-        let colorr = document.querySelector("#color")
-        
-     //==   
-
-     gridCont.addEventListener("mousedown", ()=>{
-        k = true;
-        
-       
-    }) 
-
-    gridCont.addEventListener("mouseup", ()=>{
-        k = false;
-        
-    }) 
-    gridCont.addEventListener("mouseleave", ()=>{
-        k = false;
-        
-    }) 
-   
- eraser.addEventListener("click",()=>{
-   erase = !erase
-   
- })
- 
-      //==    
+  
       togglegrid.classList.remove("red")  
-      eraser.classList.remove("red")              
+      eraser.classList.remove("red")
+      sl.classList.remove("slide-on")
+                    
      
             grids.forEach(grid =>{
+                clr.addEventListener("click",()=>{
+           
+                    grid.style.backgroundColor = "transparent"  
+                    grid.style.boxShadow = "none"    
+                })
 
                 
                 grid.addEventListener("mousedown",()=>{
-                
-                    if(erase === false){
-                    grid.style.backgroundColor = colorr.value
-                   
+                        
+                    if(erase === false && neon === true){
+                    grid.style.backgroundColor = `${colorr.value}`
+                    grid.style.boxShadow = `0 0 0px ${colorr.value}, inset  0 0 10px 3px ${colorr.value}, 0 0 20px 3px ${colorr.value}`
+                    
                    }
-                   else if(erase === true){
-                    grid.style.backgroundColor = "white"
+                   else if(erase === true ){
+                    grid.style.backgroundColor = "transparent"
+                    grid.style.boxShadow = "none"
                    }
                 })  
-            
-            
 
                 grid.addEventListener("mouseenter",()=>{
-                   
-
-                    if(k === true){
-                    grid.style.backgroundColor =colorr.value
-                    
-                    
-                    }
                     if(erase === true && k === true){
-                        grid.style.backgroundColor =`white`
+                        grid.style.backgroundColor =`transparent`;
+                        grid.style.boxShadow = "none"
+                        
                     }
+                    else if(k === true && neon === true){
+                    grid.style.backgroundColor =`${colorr.value}`;
+                    grid.style.boxShadow = `0 0 0px ${colorr.value}, inset  0 0 10px 3px ${colorr.value}, 0 0 20px 3px ${colorr.value}`
+
+                    }
+                    else if(k === true && neon === false){
+                        grid.style.backgroundColor =`${colorr.value}`;
+                        grid.style.boxShadow = "none"
+                    };
                                         
                 })     
                 
@@ -117,15 +129,7 @@ function x(defSize){
                 grid.classList.toggle("grid");      
             })
            
-        })
-        
-
-        clr.addEventListener("click",()=>{
-            grids.forEach(grid =>{
-                grid.style.backgroundColor = "transparent"      
-            })
-        })  
-        
+        })       
 
 }
 
@@ -134,9 +138,11 @@ x(16)
 
    
 newBtn.addEventListener("click",()=>{
-    x(`${newBtn.value}`)
-    para.innerText = ` ${newBtn.value} X ${newBtn.value}`
+    x(newBtn.value)
+    para.innerText = ` ${newBtn.value} X ${newBtn.value}`;
+  
 })
+
   
  
 togglegrid.addEventListener("click",()=>{
@@ -145,3 +151,11 @@ togglegrid.addEventListener("click",()=>{
 eraser.addEventListener("click",()=>{
     eraser.classList.toggle("red")
 }) 
+
+
+
+sc.addEventListener("click",()=>{
+    sl.classList.toggle("slide-on")
+})
+
+
